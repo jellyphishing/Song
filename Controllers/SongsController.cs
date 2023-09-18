dd90226c9872a8b8fb5eef45413d0895688e22d7
Lorraine Martin <lolocodes2001@gmail.com>
2023-09-18 13:28:31 -0700

    Finished adding all code to VS for API function

--- Controllers/SongsController.cs
+++ Controllers/SongsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Music_Library_Backend_Project.Data;
+using Music_Library_Backend_Project.Songs;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,36 +16,70 @@ namespace Music_Library_Backend_Project.Controllers
         {
             _context = context;
         }
-        // GET: api/<Songs>
+        // GET: api/Songs
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
-        }
+            var songs = _context.Songs.ToList();
+            return Ok(songs); 
+                }
 
-        // GET api/<Songs>/5
+        // GET api/Songs/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            var song = _context.Songs.Find(id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+            return Ok(song);
         }
 
-        // POST api/<Songs>
+        // POST api/Songs
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Song song)
         {
+            _context.Songs.Add(song);
+            _context.SaveChanges();
+            return StatusCode(201, song);
         }
 
-        // PUT api/<Songs>/5
+        // PUT api/Songs/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] Song song)
         {
+            var songToUpdate = _context.Songs.Find(id);
+            if (songToUpdate == null)
+            {
+                return NotFound();
+            }
+            songToUpdate.Title = song.Title;
+            songToUpdate.Artist = song.Artist;
+            songToUpdate.Album = song.Album;
+            songToUpdate.ReleaseDate = song.ReleaseDate;
+            songToUpdate.Genre = song.Genre;
+
+            _context.Songs.Update(songToUpdate);
+            _context.SaveChanges();
+
+            return Ok(songToUpdate);
         }
 
         // DELETE api/<Songs>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+        public IActionResult Delete(int id)
+        {   
+            var song = _context.Songs.Find(id);
+            if (song == null)
+            {
+                return NotFound(new);
+            }
+
+            _context.Songs.Remove(song);
+            _context.SaveChanges();
+            return NoContent();
+
         }
     }
 }
